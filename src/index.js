7ce0a2cbed50119914347ebb35bddeb2f01e0ed3
sosout <sosout@139.com>
2018-11-16 15:52:19 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import I18nContext from './context/I18n';
+import data from './mock/data';
 import './index.css';
-import App from './App';
+import './style/card.css';
+import CustomHookCard from './customHook/Card';
+import { useToggle, useI18n, useHash } from './customHook/Hooks';
+import ClassCard from './class/Card';
+import HookCard from './hook/Card';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Components = {
+	class: ClassCard,
+	hook: HookCard,
+	custom: CustomHookCard
+}
+
+const App = ({ data }) => {
+	let style = useHash()
+	let TargetCard = Components[style] || CustomHookCard
+	let [locale, toggleLocale] = useToggle(...data.locales)
+	console.log('style', style)
+	return (
+		<React.Fragment>
+			<I18nContext.Provider value={locale}>
+				<Local data={data} toggle={toggleLocale} />
+				<TargetCard data={data} />
+			</I18nContext.Provider>
+		</React.Fragment>
+	)
+}
+
+const Local = ({ data, toggle }) => {
+	let { locale } = useI18n(data, I18nContext)
+	return (
+		<div className="twitter__link" onClick={toggle}>
+			{locale}
+		</div>
+	)
+}
+
+ReactDOM.render(<App data={data} />, document.getElementById('root'));
+
 registerServiceWorker();
